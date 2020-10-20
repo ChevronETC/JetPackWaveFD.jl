561a5fed4ee72842c60e6c903ba9b1d16e3ff95f
Sam Kaplan <Sam.Kaplan@chevron.com>
2020-10-20 13:30:53 +0000

    remove mcells/s calculation

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,4 +1,4 @@
-using BenchmarkTools, JetPackWaveFD, Jets, LinearAlgebra
+using BenchmarkTools, JetPackWaveFD, Jets, LinearAlgebra, WaveFD
 
 _nthreads = [8]
 
@@ -10,13 +10,14 @@ n_3D = (z=parse(Int,get(ENV,"3D_NZ","101")), y=parse(Int,get(ENV,"3D_NY","102"))
 nb_2D = (z=parse(Int,get(ENV,"2D_NBZ","$(n_2D.z)")), x=parse(Int,get(ENV,"2D_NBX","8")))
 nb_3D = (z=parse(Int,get(ENV,"3D_NBZ","$(n_3D.z)")), y=parse(Int,get(ENV,"3D_NBY","8")), x=parse(Int,get(ENV,"3D_NBX","8")))
 
-SUITE["2DAcoIsoDenQ_DEO2_FDTD, no IO, forward"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoIsoDenQ_DEO2_FDTD, forward"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoIsoDenQ_DEO2_FDTD, linear"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoIsoDenQ_DEO2_FDTD, adjoint"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoIsoDenQ_DEO2_FDTD, forward, compress"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoIsoDenQ_DEO2_FDTD, linear, compress"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoIsoDenQ_DEO2_FDTD, adjoint, compress"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+ntmod = WaveFD.default_ntmod(0.004,0.001,10)
+SUITE["2DAcoIsoDenQ_DEO2_FDTD, no IO, forward"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoIsoDenQ_DEO2_FDTD, forward"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoIsoDenQ_DEO2_FDTD, linear"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoIsoDenQ_DEO2_FDTD, adjoint"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoIsoDenQ_DEO2_FDTD, forward, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoIsoDenQ_DEO2_FDTD, linear, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoIsoDenQ_DEO2_FDTD, adjoint, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
 function f2diso(nthreads,nz,nx,nbz,nbx,isfieldfile,iscompress)
     JopNlProp2DAcoIsoDenQ_DEO2_FDTD(
         b = ones(Float32,nz,nx),
@@ -48,13 +49,13 @@ for nthreads in _nthreads
     SUITE["2DAcoIsoDenQ_DEO2_FDTD, adjoint, compress"]["$nthreads threads"] = @benchmarkable mul!(δm,J',δd) setup=(F=f2diso($nthreads,$(n_2D.z),$(n_2D.x),$(nb_2D.z),$(nb_2D.x),true,true); m=1500*ones(domain(F)); d=F*m; J=jacobian!(F,m); δm=zeros(domain(J)); δm[div(n_2D.z,2),div(n_2D.x,2)] = 100; δd=J*δm) teardown=(close(F))
 end
 
-SUITE["2DAcoVTIDenQ_DEO2_FDTD, no IO, forward"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoVTIDenQ_DEO2_FDTD, forward"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoVTIDenQ_DEO2_FDTD, linear"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoVTIDenQ_DEO2_FDTD, adjoint"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoVTIDenQ_DEO2_FDTD, forward, compress"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoVTIDenQ_DEO2_FDTD, linear, compress"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoVTIDenQ_DEO2_FDTD, adjoint, compress"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoVTIDenQ_DEO2_FDTD, no IO, forward"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoVTIDenQ_DEO2_FDTD, forward"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoVTIDenQ_DEO2_FDTD, linear"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoVTIDenQ_DEO2_FDTD, adjoint"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoVTIDenQ_DEO2_FDTD, forward, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoVTIDenQ_DEO2_FDTD, linear, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoVTIDenQ_DEO2_FDTD, adjoint, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
 function f2dvti(nthreads,nz,nx,nbz,nbx,isfieldfile,iscompress)
     JopNlProp2DAcoVTIDenQ_DEO2_FDTD(
         b = ones(Float32,nz,nx),
@@ -88,13 +89,13 @@ for nthreads in _nthreads
     SUITE["2DAcoVTIDenQ_DEO2_FDTD, adjoint, compress"]["$nthreads threads"] = @benchmarkable mul!(δm,J',δd) setup=(F=f2dvti($nthreads,$(n_2D.z),$(n_2D.x),$(nb_2D.z),$(nb_2D.x),true,true); m=1500*ones(domain(F)); d=F*m; J=jacobian!(F,m); δm=zeros(domain(J)); δm[div(n_2D.z,2),div(n_2D.x,2)] = 100; δd=J*δm) teardown=(close(F))
 end
 
-SUITE["2DAcoTTIDenQ_DEO2_FDTD, no IO, forward"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoTTIDenQ_DEO2_FDTD, forward"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoTTIDenQ_DEO2_FDTD, linear"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoTTIDenQ_DEO2_FDTD, adjoint"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoTTIDenQ_DEO2_FDTD, forward, compress"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoTTIDenQ_DEO2_FDTD, linear, compress"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
-SUITE["2DAcoTTIDenQ_DEO2_FDTD, adjoint, compress"] = BenchmarkGroup([Dict("ncells"=>n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoTTIDenQ_DEO2_FDTD, no IO, forward"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoTTIDenQ_DEO2_FDTD, forward"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoTTIDenQ_DEO2_FDTD, linear"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoTTIDenQ_DEO2_FDTD, adjoint"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoTTIDenQ_DEO2_FDTD, forward, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoTTIDenQ_DEO2_FDTD, linear, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
+SUITE["2DAcoTTIDenQ_DEO2_FDTD, adjoint, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_2D.z*n_2D.x,"nbz"=>nb_2D.z,"nbx"=>nb_2D.x,"nthreads"=>_nthreads)])
 function f2dtti(nthreads,nz,nx,nbz,nbx,isfieldfile,iscompress)
     JopNlProp2DAcoTTIDenQ_DEO2_FDTD(
         b = ones(Float32,nz,nx),
@@ -129,13 +130,13 @@ for nthreads in _nthreads
     SUITE["2DAcoTTIDenQ_DEO2_FDTD, adjoint, compress"]["$nthreads threads"] = @benchmarkable mul!(δm,J',δd) setup=(F=f2dtti($nthreads,$(n_2D.z),$(n_2D.x),$(nb_2D.z),$(nb_2D.x),true,true); m=1500*ones(domain(F)); d=F*m; J=jacobian!(F,m); δm=zeros(domain(J)); δm[div(n_2D.z,2),div(n_2D.x,2)] = 100; δd=J*δm) teardown=(close(F))
 end
 
-SUITE["3DAcoIsoDenQ_DEO2_FDTD, no IO, forward"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoIsoDenQ_DEO2_FDTD, forward"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoIsoDenQ_DEO2_FDTD, linear"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoIsoDenQ_DEO2_FDTD, adjoint"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoIsoDenQ_DEO2_FDTD, forward, compress"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoIsoDenQ_DEO2_FDTD, linear, compress"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoIsoDenQ_DEO2_FDTD, adjoint, compress"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoIsoDenQ_DEO2_FDTD, no IO, forward"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoIsoDenQ_DEO2_FDTD, forward"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoIsoDenQ_DEO2_FDTD, linear"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoIsoDenQ_DEO2_FDTD, adjoint"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoIsoDenQ_DEO2_FDTD, forward, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoIsoDenQ_DEO2_FDTD, linear, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoIsoDenQ_DEO2_FDTD, adjoint, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
 function f3diso(nthreads,nz,ny,nx,nbz,nby,nbx,isfieldfile,iscompress)
     JopNlProp3DAcoIsoDenQ_DEO2_FDTD(
         b = ones(Float32,nz,ny,nx),
@@ -171,13 +172,13 @@ for nthreads in _nthreads
     SUITE["3DAcoIsoDenQ_DEO2_FDTD, adjoint, compress"]["$nthreads threads"] = @benchmarkable mul!(δm,J',δd) setup=(F=f3diso($nthreads,$(n_3D.z),$(n_3D.y),$(n_3D.x),$(nb_3D.z),$(nb_3D.y),$(nb_3D.x),true,true); m=1500*ones(domain(F)); d=F*m; J=jacobian!(F,m); δm=zeros(domain(J)); δm[div(n_3D.z,2),div(n_3D.y,2),div(n_3D.x,2)] = 100; δd=J*δm) teardown=(close(F))
 end
 
-SUITE["3DAcoVTIDenQ_DEO2_FDTD, no IO, forward"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoVTIDenQ_DEO2_FDTD, forward"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoVTIDenQ_DEO2_FDTD, linear"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoVTIDenQ_DEO2_FDTD, adjoint"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoVTIDenQ_DEO2_FDTD, forward, compress"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoVTIDenQ_DEO2_FDTD, linear, compress"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoVTIDenQ_DEO2_FDTD, adjoint, compress"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoVTIDenQ_DEO2_FDTD, no IO, forward"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoVTIDenQ_DEO2_FDTD, forward"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoVTIDenQ_DEO2_FDTD, linear"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoVTIDenQ_DEO2_FDTD, adjoint"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoVTIDenQ_DEO2_FDTD, forward, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoVTIDenQ_DEO2_FDTD, linear, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoVTIDenQ_DEO2_FDTD, adjoint, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
 function f3dvti(nthreads,nz,ny,nx,nbz,nby,nbx,isfieldfile,iscompress)
     JopNlProp3DAcoVTIDenQ_DEO2_FDTD(
         b = ones(Float32,nz,ny,nx),
@@ -215,13 +216,13 @@ for nthreads in _nthreads
     SUITE["3DAcoVTIDenQ_DEO2_FDTD, adjoint, compress"]["$nthreads threads"] = @benchmarkable mul!(δm,J',δd) setup=(F=f3dvti($nthreads,$(n_3D.z),$(n_3D.y),$(n_3D.x),$(nb_3D.z),$(nb_3D.y),$(nb_3D.x),true,true); m=1500*ones(domain(F)); d=F*m; J=jacobian!(F,m); δm=zeros(domain(J)); δm[div(n_3D.z,2),div(n_3D.y,2),div(n_3D.x,2)] = 100; δd=J*δm) teardown=(close(F))
 end
 
-SUITE["3DAcoTTIDenQ_DEO2_FDTD, no IO, forward"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoTTIDenQ_DEO2_FDTD, forward"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoTTIDenQ_DEO2_FDTD, linear"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoTTIDenQ_DEO2_FDTD, adjoint"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoTTIDenQ_DEO2_FDTD, forward, compress"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoTTIDenQ_DEO2_FDTD, linear, compress"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
-SUITE["3DAcoTTIDenQ_DEO2_FDTD, adjoint, compress"] = BenchmarkGroup([Dict("ncells"=>n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoTTIDenQ_DEO2_FDTD, no IO, forward"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoTTIDenQ_DEO2_FDTD, forward"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoTTIDenQ_DEO2_FDTD, linear"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoTTIDenQ_DEO2_FDTD, adjoint"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoTTIDenQ_DEO2_FDTD, forward, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoTTIDenQ_DEO2_FDTD, linear, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
+SUITE["3DAcoTTIDenQ_DEO2_FDTD, adjoint, compress"] = BenchmarkGroup([Dict("ncells"=>ntmod*n_3D.z*n_3D.y*n_3D.x,"nbz"=>nb_3D.z,"nby"=>nb_3D.y,"nbx"=>nb_3D.x,"nthreads"=>_nthreads)])
 function f3dtti(nthreads,nz,ny,nx,nbz,nby,nbx,isfieldfile,iscompress)
     JopNlProp3DAcoTTIDenQ_DEO2_FDTD(
         b = ones(Float32,nz,ny,nx),
@@ -260,7 +261,5 @@ for nthreads in _nthreads
     SUITE["3DAcoTTIDenQ_DEO2_FDTD, linear, compress"]["$nthreads threads"] = @benchmarkable mul!(δd,J,δm) setup=(F=f3dtti($nthreads,$(n_3D.z),$(n_3D.y),$(n_3D.x),$(nb_3D.z),$(nb_3D.y),$(nb_3D.x),true,true); m=1500*ones(domain(F)); d=F*m; J=jacobian!(F,m); δm=zeros(domain(J)); δm[div(n_3D.z,2),div(n_3D.y,2),div(n_3D.x,2)] = 100; δd=zeros(range(F))) teardown=(close(F))
     SUITE["3DAcoTTIDenQ_DEO2_FDTD, adjoint, compress"]["$nthreads threads"] = @benchmarkable mul!(δm,J',δd) setup=(F=f3dtti($nthreads,$(n_3D.z),$(n_3D.y),$(n_3D.x),$(nb_3D.z),$(nb_3D.y),$(nb_3D.x),true,true); m=1500*ones(domain(F)); d=F*m; J=jacobian!(F,m); δm=zeros(domain(J)); δm[div(n_3D.z,2),div(n_3D.y,2),div(n_3D.x,2)] = 100; δd=J*δm) teardown=(close(F))
 end
-
-include(joinpath(pkgdir(JetPackWaveFD), "benchmark", "mcells_per_second.jl"))
 
 SUITE
